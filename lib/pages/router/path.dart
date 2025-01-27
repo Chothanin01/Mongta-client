a46431167017a5813558d64c63598bc7d8a85d18
Shibaitle <shiba.despacito@gmail.com>
2025-01-28 01:35:14 +0700

    define path in path.dart

---
+++ lib/pages/router/path.dart
@@ -0,0 +1,21 @@
+class Path {
+  // Home section
+  static const String homePage = '/home';
+
+  // Entry section
+  // static const String landingPage = '/landing';
+  static const String loginPage = '/login';
+  // static const String registerPage = '/login';
+  // static const String otpPage = '/OTP';
+
+  static const String mapPage = '/map';
+
+  // รอ page ใหม่
+  // static const String Page = '/Page';
+
+  static const String chatPage = '/chat';
+  static const String settingPage = '/settings';
+  static const String scanPage = '/scan';
+
+  
+}
