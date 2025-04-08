de1a1df1668d777056cc6b96808e1c699ed12abc
Shibaitle <shiba.despacito@gmail.com>
2025-04-09 00:18:10 +0700

    Update camera_mode_button.dart

--- lib/widgets/camera_mode_button.dart
+++ lib/widgets/camera_mode_button.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:client/core/theme/theme.dart';
 
 class CameraModeButton extends StatelessWidget {
-  final bool useNativeCamera;
   final bool isDisabled;
   final VoidCallback onTap;
   
   const CameraModeButton({
     super.key,
-    required this.useNativeCamera,
     required this.onTap,
     this.isDisabled = false,
   });
@@ -23,14 +21,12 @@ class CameraModeButton extends StatelessWidget {
           color: Colors.white.withOpacity(0.85),
           shape: BoxShape.circle,
           border: Border.all(
-            color: useNativeCamera 
-                ? MainTheme.blueText 
-                : MainTheme.textfieldBorder,
+            color: MainTheme.textfieldBorder,
             width: 1.5,
           ),
         ),
         child: Icon(
-          useNativeCamera ? Icons.photo_camera : Icons.camera_alt,
+          Icons.photo_camera,
           color: isDisabled
               ? MainTheme.placeholderText
               : MainTheme.blueText,
